c6accf6526e6509135999510fbc7e56781f2d103
Michal <michal_burgunder@yahoo.com>
2020-11-21 20:16:49 +0100

    removed comments

--- performance-checker/data-analyzer.js
+++ performance-checker/data-analyzer.js
@@ -1,21 +1,15 @@
 const ss = require('simple-statistics')
 
-// const { removeAnomalies } = require('./anomaly-detection')
-
 function analyzeData(rawData, options) {
-  // const newRawData = options.anomalyDetection
-  //   ? removeAnomalies(rawData)
-  //   : rawData
-
   const finalAnalysis = {
     min: [],
     max: [],
     mean: [],
     median: [],
     variance: [],
+
     std: [],
     dataPoints: rawData.length,
-    // rawData: rawData,
   }
 
   finalAnalysis.min.push(ss.min(rawData))
