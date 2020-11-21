0c382778ede4d65070cc094bb2fcd4ee1dc465c5
Michal <michal_burgunder@yahoo.com>
2020-11-21 21:58:46 +0100

    formalied index.js

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const {
-  performanceChecker,
-} = require('./performance-checker/performance-checker')
-const { addOneMap, addOneFor, addOneForEach } = require('./functions/functions')
+const { speeder } = require('./performance-checker/performance-checker')
 
-const sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const main = async () => {
-  const theAnalysis = await performanceChecker(
-    [addOneMap.function, addOneFor.function, addOneForEach.function],
-    {
-      names: [addOneMap.name, addOneFor.name, addOneForEach.name],
-      inputs: [sampleData, sampleData, sampleData],
-    }
-  )
-  console.log(theAnalysis)
-}
+// const speeder = async () => {
+//   const theAnalysis = await performanceChecker(
+//     [addOneMap.function, addOneFor.function, addOneForEach.function],
+//     {
+//       names: [addOneMap.name, addOneFor.name, addOneForEach.name],
+//       inputs: [sampleData, sampleData, sampleData],
+//     }
+//   )
+// }
 
-main()
+module.exports = speeder
