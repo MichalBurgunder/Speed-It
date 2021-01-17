8c4ab1aa0323695f6f49280c3b2d2bda16bdf3ae
Michal <michal_burgunder@yahoo.com>
2021-01-17 10:15:27 +0100

    minor change to accomodate rounding

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ const { normalizeOptions } = require('./option-normalizer');
  *   variableInput {Boolean}:  // TODO // Whether the same input should be used in order to test cumulative behaviour
  *   raw {Boolean}: gives back the raw data (default false)
  *   counts {Number}: TODO Determines how many times the function should be run
- *   errorOutAfter {Number|[Number]}: TODO gives a percentage of how many thrown errors are accepted (assumes error is false)
- *
+ *   errorOutAfter {Number|[Number]}:  gives a percentage of how many thrown errors are accepted (assumes error is false)
+ *   verbose: {Boolean}: TODO determines whether to return just a number (mean), or return a full analysis
  * }
  */
 async function performanceChecker(theFunctions, options) {
@@ -29,7 +29,7 @@ async function performanceChecker(theFunctions, options) {
   }
   const analyzedData = [];
   for (let i = 0; i < rawData.length; i++) {
-    const analysis = await analyzeData(rawData[i], batch.options);
+    const analysis = await analyzeData(rawData[i], batch.options, i);
     analyzedData.push(analysis);
     analyzedData[i].name = batch.names[i];
   }
